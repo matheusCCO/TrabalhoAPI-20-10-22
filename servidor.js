cb8c56a78ce99a766328f7a01cfc6162fa3a5eaa
Matheus <matheus281.1@hotmail.com>
2022-10-24 16:59:23 -0300

    alterações no projeto

--- servidor.js
+++ servidor.js
@@ -1,53 +1,11 @@
-const restify = require("restify");
-const erros = require("restify-errors");
+const express = require('express');
+const server = express();
+const filmes = require('./filmes.json');
 
-const servidor =restify.createServer({
-    nome: 'pokemon',
-    version: '1.0.0'
+server.get('/filmes', (req, res) => {
+    return res.json(filmes);
 });
 
-servidor.use(restify.plugins.acceptParser(servidor.acceptable));
-servidor.use(restify.plugins.queryParser() );
-servidor.use(restify.plugins.bodyParser() );
-
-servidor.listen(8001, ()=>{
-    console.log("%s executando em %s", servidor.name, servidor.url)
+server.listen(3000, () => {
+    console.log('servidor iniciado');
 });
-
-servidor.get("/", (req,res)=>{
-    res.send("Bem-vindosdfsdf");
-});
-
-const knex = require("knex")({
-    client : 'mysql',
-    connection : {
-        host : 'localhost',
-        user : 'root',
-        password : '',
-        database : 'pokemon'
-    },
-});
-
-servidor.get("/pokemon", (req,res, next)=>{
-    
-    knex('pokemon').then((dados)=>{
-       
-        res.send(dados);
-    }, next);
-});
-
-
-function adicionaPoke(){
-    var nome = document.getElementById("nome").value;
-    servidor.post("/pokemon",( req, res, next)=>{
-    
-        knex('pokemon')
-        .insert({nome:nome})
-        .then((dados)=>{
-            if(!dados){
-                return res.send(new erros.BadRequestError('Erro ao inserir o Pokemon'));
-            }
-            res.send("Pokemon inserido");
-        }, next);
-    });
-}
